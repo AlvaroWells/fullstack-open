dcaac6ea69e3bade2d669ce51104b6934b6c90fe
Alvaro Wells <alvarogwells@gmail.com>
2024-03-20 16:05:24 +0100

    manejo de errores de app notes

--- part3/part3-notes-backend-main/index.js
+++ part3/part3-notes-backend-main/index.js
@@ -1,29 +1,40 @@
-//lineas de importación de librerías JS
-//express para crear el servidor
 const express = require('express')
-//cors para las políticas middleware
-const cors = require('cors')
-//variables de entorno
-require('dotenv').config()
-//nombre de la aplicación
 const app = express()
-//importamos el módulo Note
+require('dotenv').config()
+
 const Note = require('./models/note')
 
-let notes = [
-]
-
-app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
-//funcion para generar una nueva id de las notas
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+//función middleware de Express con funcionalidad de control de errores
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+    
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+    
+  next(error)
+}
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+// funcion para controlar solicitudes con endpoint desconocido
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 //métodos get
 app.get('/', (req, res) => {
@@ -34,17 +45,22 @@ app.get('/api/notes', (req, res) => {
   Note
     .find({})
     .then(notes => {
-      console.log(notes)
+      // console.log(notes)
       res.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note
     .findById(request.params.id)
     .then(note => {
-      response.json(note)
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 //metodo post
@@ -70,12 +86,36 @@ app.post('/api/notes', (request, response) => {
 })
 
 //metodo delete
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+//método put
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note
+    .findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+// controlador de solicitudes con errores del cliente
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
