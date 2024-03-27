5d66b23d7d84cc7decae4b41777f35525b4e3875
Alvaro Wells <alvarogwells@gmail.com>
2024-03-27 11:07:23 +0100

    added .catch in get route

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -2,12 +2,13 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogRouter.get('/', (request, response) => {
+blogRouter.get('/', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => next(error))
 })
 
 blogRouter.post('/', (request, response, next) => {
