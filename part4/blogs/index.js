a56f1d2dcb08128d2b34ecf90b55e4ff127041cd
Alvaro Wells <alvarogwells@gmail.com>
2024-03-27 11:07:03 +0100

    cleaned index.js blog app

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -3,26 +3,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
