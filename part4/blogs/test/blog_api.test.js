a003a66cd4297b2de9b32970cbeeac8d1ed7c5a9
Alvaro Wells <alvarogwells@gmail.com>
2024-04-07 16:17:45 +0200

    describe for testing exercises

--- part4/blogs/test/blog_api.test.js
+++ part4/blogs/test/blog_api.test.js
@@ -21,82 +21,86 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-type', /application\/json/)
-}, 100000)
+describe('Blog API', () => {
+  describe('GET /api/blogs', () => {
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-type', /application\/json/)
+    }, 100000)
 
-test('blog returns id parameter', async () => {
-  const ids = await helper.findId()
-  expect(ids).toBeDefined()
+    test('blog returns id parameter', async () => {
+      const ids = await helper.findId()
+      expect(ids).toBeDefined()
+    })
+  })
+
+  describe('POST /api/blogs', () => {
+    test('a new blog can be added', async () => {
+      const response = await helper.blogsInDb()
+      const newBlog = response[0]
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(response.length + 1)
+
+      const titles = blogsAtEnd.map(b => b.title)
+      expect(titles).toContain(
+        `${newBlog.title}`
+      )
+    })
+
+    test('if likes property is missing, it defaults to 0', async () => {
+      let newBlog = {
+        title: 'newblog',
+        author: 'yourself',
+        url: 'https://whatever.rer',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await helper.blogsInDb()
+      // console.log(response)
+      newBlog = response.find(b => b.likes === undefined)
+      helper.addObjectLikes(newBlog, 0)
+
+      const likes = await helper.findLike()
+      expect(likes).toBeDefined()
+      expect(newBlog.likes).toBe(0)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    })
+
+    test('if title or url is missing, it returns 400 error', async () => {
+      let newBlog = {
+        title: '',
+        author: 'author',
+        url: '',
+        likes: 12
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+  })
 })
-
-test('a new blog can be added', async () => {
-  const response = await helper.blogsInDb()
-  const newBlog = response[0]
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(response.length + 1)
-
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain(
-    `${newBlog.title}`
-  )
-})
-
-test('if likes property is missing, it defaults to 0', async () => {
-  let newBlog = {
-    title: 'newblog',
-    author: 'yourself',
-    url: 'https://whatever.rer',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await helper.blogsInDb()
-  // console.log(response)
-  newBlog = response.find(b => b.likes === undefined)
-  helper.addObjectLikes(newBlog, 0)
-
-  const likes = await helper.findLike()
-  expect(likes).toBeDefined()
-  expect(newBlog.likes).toBe(0)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-})
-
-test('if title or url is missing, it returns 400 error', async () => {
-  let newBlog = {
-    title: '',
-    author: 'author',
-    url: '',
-    likes: 12
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
 
 afterAll(() => {
   mongoose.connection.close()
