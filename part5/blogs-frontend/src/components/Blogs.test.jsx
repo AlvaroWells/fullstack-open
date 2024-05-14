40ce4cb99733f51f21341f47913ef00c89086162
Alvaro Wells <alvarogwells@gmail.com>
2024-05-14 10:18:31 +0200

    5.15 testing blog app exercise

--- part5/blogs-frontend/src/components/Blogs.test.jsx
+++ part5/blogs-frontend/src/components/Blogs.test.jsx
@@ -26,6 +26,8 @@ describe('<Blogs />', () => {
     expect(screen.queryByText('likes:')).not.toBeInTheDocument()
   })
 
+  // 5.14: Pruebas de Listas de Blogs, paso 2
+  // Realiza una prueba que verifique que la URL del blog y el número de likes se muestran cuando se hace clic en el botón que controla los detalles mostrados.
   test('after clicking the "view" button, URL and likes are displayed', () => {
     const blogs = [
       {
@@ -38,7 +40,14 @@ describe('<Blogs />', () => {
       }
     ]
 
-    render(<Blogs blogs={blogs} updateBlogLikes={() => {}} deleteBlog={() => {}} user={{ username: 'testuser' }} />)
+    render(
+      <Blogs
+        blogs={blogs}
+        updateBlogLikes={() => {}}
+        deleteBlog={() => {}}
+        user={{ username: 'testuser' }}
+      />
+    )
 
     // Find the "view" button and click it
     const viewButton = screen.getByText('view')
@@ -52,5 +61,32 @@ describe('<Blogs />', () => {
     const likesElement = screen.getByText('likes: 5')
     expect(urlElement).toBeInTheDocument()
     expect(likesElement).toBeInTheDocument()
+  })
+
+  // 5.15: Pruebas de Listas de Blogs, paso 3
+  // Realiza una prueba que garantice que si se hace clic dos veces en el botón like, se llama dos veces al controlador de eventos que el componente recibió como props.
+
+  test('When clicking twice the like button it responds correctly with the information', async () => {
+    const blog = {
+      id: 1,
+      title: 'Testing blog',
+      author: 'Testing author',
+      url: 'http://test.com',
+      likes: 5,
+      user: { username: 'testuser' }
+    }
+
+    const mockHandler = vi.fn()
+
+    render(
+      <Blogs blogs={[blog]} updateBlogLikes={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
